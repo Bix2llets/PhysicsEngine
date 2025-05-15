567bb0653aa087d9ff2d65eeb46a075df694b5ad
Bix2llets <nhtphat2418@apcs.fitus.edu.vn>
2025-05-15 23:04:04 +0700

    Add pragma once. Change the type of parameter of method processEvent of Scene

--- include/Scene/scene.h
+++ include/Scene/scene.h
@@ -1,7 +1,7 @@
+#pragma once
 #include <SFML/Graphics.hpp>
 
-#include "GUIObject.h"
-
+#include <optional>
 class Scene {
    protected:
     sf::RenderWindow &window;
@@ -13,6 +13,6 @@ class Scene {
    public:
     virtual void render() = 0;
     virtual void update() = 0;
-    virtual void processEvent() = 0;
+    virtual void processEvent(std::optional<sf::Event> &event) = 0;
     virtual void processInput() = 0;
 };
